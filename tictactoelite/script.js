9a97cc268e93922f9e9eb92268b61dc7df02dac1
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-02-18 19:36:42 +0100

    Add light version of TicTacToe

---
+++ tictactoelite/script.js
@@ -0,0 +1,35 @@
+"use strict";
+let player = false;
+let won = false;
+
+document.getElementById("field").addEventListener("click", function(e) {
+    if (!won && Number(e.srcElement.dataset.type)) {
+        e.srcElement.dataset.type = player ? "o" : "x";
+		var rows = document.getElementById('field').rows;
+        for (let i = 0; i < 3; i++) {
+			if (rows[i].cells[0].dataset.type === rows[i].cells[1].dataset.type && rows[i].cells[0].dataset.type === rows[i].cells[2].dataset.type) { // Horizontal
+				won = true;
+			} else if (rows[0].cells[i].dataset.type === rows[1].cells[i].dataset.type && rows[0].cells[i].dataset.type === rows[2].cells[i].dataset.type) { // Vertikal
+				won = true;
+			}
+		}
+		if (rows[0].cells[0].dataset.type === rows[1].cells[1].dataset.type && rows[1].cells[1].dataset.type === rows[2].cells[2].dataset.type) { // Diagonal \
+			won = true;
+		} else if (rows[0].cells[2].dataset.type === rows[1].cells[1].dataset.type && rows[1].cells[1].dataset.type === rows[2].cells[0].dataset.type) { // Diagonal /
+			won = true;
+		}
+		if (won) alert((player ? 'o' : 'x') + ' won');
+		if (!won) player = !player;
+    }
+});
+
+document.getElementById("button").addEventListener("click", function() {
+	var rows = document.getElementById('field').rows;
+	for (var i=0; i<rows.length; i++) {
+		for (var j=0; j<rows[i].cells.length; j++) {
+			rows[i].cells[j].dataset.type = 3 * i + j + 1;
+		}
+	}
+	if (won) player = !player;
+	won = false;
+});
