190d9207d1def877058315b26dd1753bf9527f17
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-02-18 12:49:07 +0100

    fix?

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -32,8 +32,5 @@ document.getElementById('button').addEventListener('click', function() {
 })
 
 if ('serviceWorker' in navigator) {
-	// Use the window load event to keep the page load performant
-	window.addEventListener('load', function() {
-		navigator.serviceWorker.register('service-worker.js', { scope: "./" });
-	});
+	navigator.serviceWorker.register('/service-worker.js', { scope: './' });
 }
