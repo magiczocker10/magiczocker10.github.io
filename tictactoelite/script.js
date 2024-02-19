2b6958a9e33c04d481a38d03db2519128e52a994
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-02-19 20:35:56 +0100

    Small changes

--- tictactoelite/script.js
+++ tictactoelite/script.js
@@ -1,34 +1,39 @@
-"use strict";
+'use strict';
 var player = false;
 var won = false;
 
-document.getElementById("field").addEventListener("click", function(e) {
-    if (!won && Number(e.srcElement.dataset.type)) {
-        e.srcElement.dataset.type = player ? "o" : "x";
+function getType(ele) {
+	return ele.getAttribute('data-type');
+}
+
+document.getElementById('field').addEventListener('click', function(e) {
+	var type = getType(e.target);
+    if (!won && Number(type)) {
+        e.target.setAttribute('data-type', (player ? 'o' : 'x'));
 		var rows = document.getElementById('field').rows;
         for (var i = 0; i < 3; i++) {
-			if (rows[i].cells[0].dataset.type === rows[i].cells[1].dataset.type && rows[i].cells[0].dataset.type === rows[i].cells[2].dataset.type) { // Horizontal
+			if (getType(rows[i].cells[0]) === getType(rows[i].cells[1]) && getType(rows[i].cells[0]) === getType(rows[i].cells[2])) { // Horizontal
 				won = true;
-			} else if (rows[0].cells[i].dataset.type === rows[1].cells[i].dataset.type && rows[0].cells[i].dataset.type === rows[2].cells[i].dataset.type) { // Vertikal
+			} else if (getType(rows[0].cells[i]) === getType(rows[1].cells[i]) && getType(rows[0].cells[i]) === getType(rows[2].cells[i])) { // Vertikal
 				won = true;
 			}
 		}
-		if (rows[0].cells[0].dataset.type === rows[1].cells[1].dataset.type && rows[1].cells[1].dataset.type === rows[2].cells[2].dataset.type) { // Diagonal \
+		if (getType(rows[0].cells[0]) === getType(rows[1].cells[1]) && getType(rows[1].cells[1]) === getType(rows[2].cells[2])) { // Diagonal \
 			won = true;
-		} else if (rows[0].cells[2].dataset.type === rows[1].cells[1].dataset.type && rows[1].cells[1].dataset.type === rows[2].cells[0].dataset.type) { // Diagonal /
+		} else if (getType(rows[0].cells[2]) === getType(rows[1].cells[1]) && getType(rows[1].cells[1]) === getType(rows[2].cells[0])) { // Diagonal /
 			won = true;
 		}
 		if (won) alert((player ? 'o' : 'x') + ' won');
 		player = !player;
     }
-});
+}, false);
 
-document.getElementById("button").addEventListener("click", function() {
+document.getElementById('button').addEventListener('click', function() {
 	var rows = document.getElementById('field').rows;
 	for (var i=0; i<rows.length; i++) {
 		for (var j=0; j<rows[i].cells.length; j++) {
-			rows[i].cells[j].dataset.type = 3 * i + j + 1;
+			rows[i].cells[j].setAttribute('data-type', (3 * i + j + 1));
 		}
 	}
 	won = false;
-});
+}, false);
