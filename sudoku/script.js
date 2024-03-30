0c88b2be8483d47c55627f3b49f99d8f24f2e7bb
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-03-30 08:00:20 +0100

    Fix sudoku on 3DS

--- sudoku/script.js
+++ sudoku/script.js
@@ -1,33 +1,41 @@
 /*
 	Sizes 4x4, 6x6, 9x9
 */
-// jshint jquery:false, browser:true, devel:true, camelcase:true, curly:false, eqeqeq:true, esversion: 5, forin:true, freeze:true, immed:true, latedef:true, leanswitch:true, newcap:true, noarg:true, regexp:true, strict:true, trailing:false, undef:true, unused:true
 window.addEventListener('load', function() {
-	"use strict";
-	var t = document.querySelector("#board");
-	var wn = document.querySelector('#winnotifier');
-	var sizes = {4: [2, 2, 10], 6: [3, 2, 21], 9: [3, 3, 45]}, // Width, Height, Sum per col/row
+	'use strict';
+	var t = document.getElementById('board'),
+		wn = document.getElementById('winnotifier'),
+		sizes = {4: [2, 2, 10], 6: [3, 2, 21], 9: [3, 3, 45]}, // Width, Height, Sum per col/row
 		field = [],
-		size = 9;
-	var last;
+		size = 9,
+		last;
 	function getSumOfColOrRow(n, isCol) {
 		var c = 0;
-		t.querySelectorAll(isCol ? 'td:nth-child(' + n + ')' : 'tr:nth-child(' + n + ') td').forEach(function(e) {
-			c += Number(e.innerText);
-		});
+		if (isCol) {
+			var r = t.rows;
+			for (var i=0; i<r.length; i++) {
+				c += Number(r[i].children[n].textContent);
+			}
+			
+		} else {
+			var r = t.rows[n].children;
+			for (var i=0; i<r.length; i++) {
+				c += Number(r[i].textContent);
+			}
+		}
 		return c;
 	}
 	function checkWin() {
 		for (var i = 0; i < size; i++) {
-			if (getSumOfColOrRow(i + 1, false) !== sizes[size][2]) {return false;}
-			if (getSumOfColOrRow(i + 1, true) !== sizes[size][2]) {return false;}
+			if (getSumOfColOrRow(i, false) !== sizes[size][2]) {return false;}
+			if (getSumOfColOrRow(i, true) !== sizes[size][2]) {return false;}
 		}
-		wn.innerText = "You won!";
+		wn.textContent = 'You won!';
 		return true;
 	}
 	function generateField(size) {
 		var a = ('123456789').substring(0, size);
-		a = a.repeat(4);
+		a = a + a + a + a;
 		for (var i = 0; i < size; i++) {
 			var offset = Math.ceil((i + 1) / size * sizes[size][0]) - 1;
 			field[i] = a.substring(i * sizes[size][0] + offset, i * sizes[size][0] + size + offset).split("");
@@ -66,7 +74,7 @@ window.addEventListener('load', function() {
 		}
 	}
 	function generateTable(size) {
-		t.innerText='';
+		t.textContent='';
 		for (var i = 0; i < size; i++) {
 			var row = t.insertRow();
 			for (var j = 0; j < size; j++) {
@@ -80,16 +88,16 @@ window.addEventListener('load', function() {
 		for (var i = 0; i < n; i++) {
 			var x = Math.floor(Math.random() * size);
 			var y = Math.floor(Math.random() * size);
-			r[y].cells[x].innerText = "";
+			r[y].cells[x].innerText = '';
 			r[y].cells[x].classList.add('input');
 		}
 	}
 	function reset() {
 		wn.innerText = '';
-		size = Number(document.getElementById("sizeselector").selectedOptions[0].dataset.size);
+		size = Number(document.getElementById('sizeselector').selectedOptions[0].dataset.size);
 		t.setAttribute('data-size', size);
 		var i = document.getElementById('input');
-		i.innerText='';
+		i.textContent='';
 		var s = sizes[size];
 		for (var j = 0; j < s[1]; j++) {
 			var r = i.insertRow();
@@ -104,9 +112,9 @@ window.addEventListener('load', function() {
 		randomizeField(switchCol, sizes[size][0], sizes[size][1]);
 		randomizeField(switchRow, sizes[size][1], sizes[size][0]);
 		generateTable(size);
-		replaceRandomEntries(Number(document.getElementById("diffselector").selectedOptions[0].dataset.emptycells.split(",")[document.getElementById("sizeselector").selectedIndex]));
+		replaceRandomEntries(Number(document.getElementById('diffselector').selectedOptions[0].dataset.emptycells.split(",")[document.getElementById("sizeselector").selectedIndex]));
 	}
-	document.getElementById("resetbtn").addEventListener('click', reset);
+	document.getElementById('resetbtn').addEventListener('click', reset);
 	document.getElementById('board').addEventListener('click', function(e) {
 		if (e.target.nodeName !== 'TD' || !e.target.classList.contains('input')) return;
 
@@ -120,7 +128,7 @@ window.addEventListener('load', function() {
 		last.textContent = e.target.textContent;
 		checkWin();
 	});
-	document.addEventListener("keydown", function(e) {
+	document.addEventListener('keydown', function(e) {
 		if (e.isComposing || e.key === 229) { return; }
 		if (e.key > 0 && e.key <= size) {
 			last.textContent = e.key;
