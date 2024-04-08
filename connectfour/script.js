fc832d785b9fcc60376f3eed47c3fffd05192e85
magiczocker10 <59317952+magiczocker10@users.noreply.github.com>
2024-04-08 18:55:03 +0200

    Update connect 4

--- connectfour/script.js
+++ connectfour/script.js
@@ -1,78 +1,92 @@
 window.addEventListener('load', function() {
-	var player = false;
-	var won = false;
-	var field = document.getElementById('field').tBodies[0].rows;
+	var player = false,
+		won = false,
+		field = document.getElementById('field'),
+		rows = field.tBodies[0].rows;
 
 	function getType(x, y) {
-		return field[y].cells[x].getAttribute('data-type');
+		return rows[y].cells[x].getAttribute('data-type');
 	}
 
-	function checkWin() {
-		// horizontal
-		for (var row = 0; row < 6; row++) {
-			for (var column = 0; column < 4; column++) {
-				if ((getType(column, row) === getType(column + 1, row)) &&
-					(getType(column, row) === getType(column + 2, row)) &&
-					(getType(column, row) === getType(column + 3, row)) &&
-					(getType(column, row) !== '0')) {
-					won = true;
-				}
+	function checkWin(x, y) {
+		for (var n=0; n<4; n++) {
+			const t = getType(n, y);
+
+			// horizontal
+			if ((t === getType(n + 1, y)) &&
+				(t === getType(n + 2, y)) &&
+				(t === getType(n + 3, y)) &&
+				(t !== '')) {
+				won = true;
+			}
+
+			if (n === 3) break;
+
+			const t2 = getType(x, n);
+
+			// vertical
+			if ((t2 === getType(x, n + 1)) &&
+				(t2 === getType(x, n + 2)) &&
+				(t2 === getType(x, n + 3)) &&
+				(t2 !== '')) {
+				won = true;
 			}
 		}
 
-		// vertical
-		for (var column = 0; column < 7; column++) {
-			for (var row = 0; row < 3; row++) {
-				if ((getType(column, row) === getType(column, row + 1)) &&
-					(getType(column, row) === getType(column, row + 2)) &&
-					(getType(column, row) === getType(column, row + 3)) &&
-					(getType(column, row) !== '0')) {
-					won = true;
-				}
-			}
-		}
-
-		for (var row = 0; row < 3; row++) {
-			for (var column = 0; column < 4; column++) {
+		for (var row2 = 0; row2 < 3; row2++) {
+			for (var column2 = 0; column2 < 4; column2++) {
 				// diagonal \
-				if ((getType(column, row + 3) === getType(column + 1, row + 2)) &&
-					(getType(column, row + 3) === getType(column + 2, row + 1)) &&
-					(getType(column, row + 3) === getType(column + 3, row)) &&
-					(getType(column, row + 3) !== '0')) {
+				const t = getType(column2, row2 + 3),
+					t2 = getType(column2, row2);
+				if ((t === getType(column2 + 1, row2 + 2)) &&
+					(t === getType(column2 + 2, row2 + 1)) &&
+					(t === getType(column2 + 3, row2)) &&
+					(t !== '')) {
 					won = true;
 				} else
 
 				// diagonal /
-				if ((getType(column, row) === getType(column + 1, row + 1)) &&
-					(getType(column, row) === getType(column + 2, row + 2)) &&
-					(getType(column, row) === getType(column + 3, row + 3)) &&
-					(getType(column, row) !== '0')) {
+				if ((t2 === getType(column2 + 1, row2 + 1)) &&
+					(t2 === getType(column2 + 2, row2 + 2)) &&
+					(t2 === getType(column2 + 3, row2 + 3)) &&
+					(t2 !== '')) {
 					won = true;
 				}
 			}
 		}
 	}
 
-	document.getElementById('field').addEventListener('click', function(e) {
-		if (!won && e.target.tagName === 'TH') {
-			var column = e.target.cellIndex;
-			for (var row = 5; row >= 0; row--) {
-				if (getType(column, row) === '0') {
-					field[row].cells[e.target.cellIndex].setAttribute('data-type', (player ? 2 : 1));
-					checkWin();
-					if (won) alert((player ? 'black' : 'white') + ' won');
-					player = !player;
-					break;
-				}
-			}
+	function reset() {
+		var c = field.tHead.rows[0].cells;
+		for (var j=0; j<c.length; j++) {
+			c[j].style.color = '';
+			c[j].setAttribute('data-index', '5');
 		}
-	}, false);
 
-	document.getElementById('button').addEventListener('click', function() {
-		var cells = document.getElementsByTagName('td');
+		var cells = field.getElementsByTagName('td');
 		for (var i=0; i<cells.length; i++) {
-			cells[i].setAttribute('data-type', 0);
+			cells[i].setAttribute('data-type', '');
 		}
 		won = false;
-	}, false);
+	}
+
+	field.addEventListener('click', function(e) {
+		if (won || e.target.tagName !== 'TH') return;
+
+		const column = e.target.cellIndex;
+			row = Number(e.target.getAttribute('data-index'));
+
+		if (row < 0) return;
+		if (row === 0) e.target.style.color = 'transparent';
+
+		rows[row].cells[e.target.cellIndex].setAttribute('data-type', (player ? '2' : '1'));
+		checkWin(column, row);
+		if (won) alert((player ? 'black' : 'white') + ' won');
+		player = !player;
+		e.target.setAttribute('data-index', row - 1);
+	});
+
+	document.getElementById('button').addEventListener('click', reset);
+
+	reset();
 });
